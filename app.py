c27425e440ca64b3c05908b93b6d7fa503ba09be
unknown <sealboop@gmail.com>
2026-05-23 21:45:35 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import numpy as np
+from methods.gaussian import parse_input, gauss_elim
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        eq1 = request.form["eq1"]
+        eq2 = request.form["eq2"]
+        eq3 = request.form["eq3"]
+
+        A = []
+        B = []
+
+        for eq in [eq1, eq2, eq3]:
+            coeffs, constant = parse_input(eq)
+            A.append(coeffs)
+            B.append([constant])
+
+        A = np.array(A, dtype=object)
+        B = np.array(B, dtype=object)
+
+        result = gauss_elim(A, B)
+
+        return render_template("result.html", result=result)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ methods/gaussian.py
@@ -0,0 +1,51 @@
+import numpy as np
+from fractions import Fraction
+
+def parse_input(eq):
+    eq = eq.replace(" ", "")
+    left, right = eq.split("=")
+    constant = Fraction(right)
+
+    coeffs = {'x': 0, 'y': 0, 'z': 0}
+    left = left.replace("-", "+-")
+    terms = left.split("+")
+
+    for term in terms:
+        if term == "":
+            continue
+        if 'x' in term:
+            coeff = term.replace("x", '')
+            coeffs['x'] = Fraction(coeff) if coeff not in ["", "+"] else 1
+            if coeff == "-":
+                coeffs['x'] = -1
+        elif 'y' in term:
+            coeff = term.replace("y", '')
+            coeffs['y'] = Fraction(coeff) if coeff not in ["", "+"] else 1
+            if coeff == "-":
+                coeffs['y'] = -1
+        elif 'z' in term:
+            coeff = term.replace("z", '')
+            coeffs['z'] = Fraction(coeff) if coeff not in ["", "+"] else 1
+            if coeff == "-":
+                coeffs['z'] = -1
+
+    return [coeffs['x'], coeffs['y'], coeffs['z']], constant
+
+
+def gauss_elim(A, B):
+    n = len(B)
+    augmented = np.concatenate((A, B), axis=1)
+
+    # Forward elimination
+    for i in range(n):
+        for j in range(i+1, n):
+            factor = augmented[j][i] / augmented[i][i]
+            augmented[j] -= factor * augmented[i]
+
+    # Back substitution
+    x = np.zeros(n, dtype=object)
+    for i in range(n-1, -1, -1):
+        sum_ax = sum(augmented[i][j] * x[j] for j in range(i+1, n))
+        x[i] = (augmented[i][n] - sum_ax) / augmented[i][i]
+
+    return x
